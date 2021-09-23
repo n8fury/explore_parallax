f68c07cc4c59a74ebdc587984f124470ac7dde88
Mamdud Hasan <mhjoy547@yahoo.com>
2021-09-24 04:19:15 +0600

    Final Build
    Project Complete

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ let timeline = new TimelineMax();
 timeline
   .to(".rock1", 3, { y: -300, })
   .to(".girl1", 3, { y: -200, }, "-=3")
-  .to(".bg1", 3, { y: 50, }, "-=3");
+  .fromTo(".bg1", 3, { y: -50, }, { y: 0, duration: 3 }, "-=3")
+  .to(".content", 3, { top: '0%' }, "-=3")
+  .fromTo(".content-image", { opacity: 0 }, { opacity: 1, duration: 3 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
-  duration: "100%",
+  duration: "150%",
   triggerHook: 0,
 })
   .setTween(timeline)
+  .setPin("section")
   .addTo(controller);
 
 
