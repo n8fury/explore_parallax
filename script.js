223d2d1994146564d07c65177120977b9f74a2b7
Mamdud Hasan <mhjoy547@yahoo.com>
2021-09-24 03:58:04 +0600

    animated main section
    completed parallax animation in the main section

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
- let controller = new ScrollMagic.Controller();
- let timeline = new TimelineMax();
- timeline
- .to(".text", 3, {
-   x: 250
- })
- .to(".text", 2, {
-   opacity: 0
- },"-2");
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+timeline
+  .to(".rock1", 3, { y: -300, })
+  .to(".girl1", 3, { y: -200, }, "-=3")
+  .to(".bg1", 3, { y: 50, }, "-=3");
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "100%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .addTo(controller);
+
+
